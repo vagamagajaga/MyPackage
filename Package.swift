7ed67b51708603781655167169ecf9da65f87d69
vagamagajaga <gvagan@me.com>
2023-12-06 15:47:26 +0700

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "MyPackage",
+    platforms: [
+        .iOS(.v15)], 
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,12 +14,7 @@ let package = Package(
             targets: ["MyPackage"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPackage"),
-        .testTarget(
-            name: "MyPackageTests",
-            dependencies: ["MyPackage"]),
+            name: "MyPackage")
     ]
 )
--- Sources/MyPackage/MyPackage.swift
+++ Sources/MyPackage/MyPackage.swift
@@ -1,2 +1,17 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import UIKit
+
+extension Int {
+    func intToStringWithSeparator() -> String {
+        let numberFormatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.groupingSeparator = " "
+            return formatter
+        }()
+        let num = numberFormatter.string(from: self as NSNumber) ?? "0"
+        return num
+    }
+}
+
+
